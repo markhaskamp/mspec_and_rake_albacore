873ca25c0a5cbdd215cf2645609b67bfacb1d86c
Mark Haskamp <mrhask@gmail.com>
2010-07-23 17:40:45 -0400

    working tests

--- app/Operand/Operand.cs
+++ app/Operand/Operand.cs
@@ -6,5 +6,13 @@ namespace Operand
         public decimal Add(decimal firstOp, decimal secondOp) {
             return firstOp + secondOp;
         }
+
+        public decimal Add(params decimal[] operands) {
+            decimal returnValue = 0m;
+            foreach (decimal operand in operands) {
+                returnValue += operand;
+            }
+            return returnValue;
+        }
     }
 }
--- test/Operand_Test/Operand_Test.cs
+++ test/Operand_Test/Operand_Test.cs
@@ -5,6 +5,16 @@ namespace Operand_Test
     [Subject("Operator")]
     public class Add
     {
-        private It should_add_two_operands = () => new Operand.Operand().Add(42, 42).ShouldEqual(84.0m);
+        private It should_add_two_operands = () =>
+                     {
+                        Operand.Operand o = new Operand.Operand();
+                         o.Add(42, 42).ShouldEqual(84.0m);
+                     };
+
+        private It should_add_all_operands = () =>
+                     {
+                         Operand.Operand o = new Operand.Operand();
+                         o.Add(1m, 2m, 3m).ShouldEqual(6m);
+                     };
     }
 }
