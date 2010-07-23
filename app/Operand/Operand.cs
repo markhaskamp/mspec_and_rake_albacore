aa806df2bec2b3cf36e8193c5cae53e98d671e0d
Mark Haskamp <mrhask@gmail.com>
2010-07-23 17:17:50 -0400

    added test project with an mspec file

--- app/Operand/Operand.cs
+++ app/Operand/Operand.cs
@@ -3,5 +3,8 @@ namespace Operand
 {
     public class Operand
     {
+        public decimal Add(decimal firstOp, decimal secondOp) {
+            return firstOp + secondOp;
+        }
     }
 }
---
+++ test/Operand_Test/Operand_Test.cs
@@ -0,0 +1,10 @@
+﻿using Machine.Specifications;
+
+namespace Operand_Test
+{
+    [Subject("Operator")]
+    public class Add
+    {
+        private It should_add_two_operands = () => new Operand.Operand().Add(42, 42).ShouldEqual(84.0m);
+    }
+}
